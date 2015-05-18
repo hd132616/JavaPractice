3cecf0b9facb9b3d9c3900d58823d2c70279b085
Administrator <Administrator@MSDN-SPECIAL>
2015-05-18 16:30:33 +0900

    10번째 실습과제 입니다:)

---
+++ JavaPractice/src/org/dimigo/oop/SnackTest.java
@@ -0,0 +1,17 @@
+package org.dimigo.oop;
+
+public class SnackTest {
+	public static void main(String[] args){
+		Snack[] snack = new Snack[3];
+		snack[0] = new Snack("새우깡","농심",1100,2);
+		snack[1] = new Snack("콘칲","크라운",1200,1);
+		snack[2] = new Snack("허니버터칩","해태",1500,4);
+		int sum=0;
+		for(int i=0; i<snack.length; ++i){
+			snack[i].printSnack();
+			sum += (snack[i].price)*(snack[i].number);
+		}
+		System.out.println();
+		System.out.println("총 구매 금액 : " + String.format("%,d",sum) +"원");
+	}
+}
